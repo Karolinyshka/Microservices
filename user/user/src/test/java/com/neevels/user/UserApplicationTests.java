e725de4a6ba97616ae493bcb001702b3286e8c23
nevels <neevels@mail.ru>
2023-01-08 21:16:13 +0300

    Initial commit

---
+++ user/user/src/test/java/com/neevels/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neevels.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
